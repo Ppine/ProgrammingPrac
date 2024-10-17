1c0d64e5575e15db31da9e0e1fa7dd17e44ec462
Ppine <tomy050213@naver.com>
2024-10-17 14:18:22 +0900

    beta ver 1017

---
+++ 2024-14090_박영민(5차시)/hw1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    long double x,y;
+    long double X,Y;
+    long double distance;
+    int m=0;
+    long double pi;
+    
+    scanf("%d",&n);
+    srand(42);//set seed
+
+    for (int i=0; i<n; i++)
+    {
+        x = (long double)rand()/(long double)(RAND_MAX);
+        // printf("%d",RAND_MAX);
+        y = (long double)rand()/(long double)(RAND_MAX);
+        // printf("%.10f\n%.10f\n",x,y);
+        // printf("x\n");
+        X = pow(x,2);
+        Y = pow(y,2);
+        // printf("%.10f\n%.10f\n",X,Y);
+        long double xy = X+Y;
+        distance = sqrt(xy);
+        if(distance <= 1) m++;
+    }
+    pi = 4 * ((long double)m/(long double)n);
+    // printf("%d %d\n",m,n);
+    
+    // printf("%lf",(double)m/(double)n);
+
+    printf("%0.6Lf",pi);
+    
+    return 0;
+}
---
+++ 2024-14090_박영민(5차시)/hw2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main()
+{
+    int n;
+    double sin_value;
+    double ran_value;
+    double max = 0;
+    double min = 100.0;
+    double sum = 0;
+
+    scanf("%d",&n);
+    srand(42);
+
+    for(int i=0; i<n; i++)
+    {   
+        ran_value = (double)rand()/(double)(RAND_MAX);// set random value [0,1]
+        // printf("%lf\n",ran_value);
+        ran_value  = (ran_value * M_PI) - M_PI_2; // set random value [-pi/2 , pi/2] (* pi -pi/2)
+        // printf("%lf\n\n",ran_value);
+        sin_value = fabs(sin(ran_value));
+        // printf("%lf ",sin_value);
+        if(sin_value >= max) max = sin_value; //printf("max = %lf",max);
+        if(sin_value <= min) min = sin_value; //printf("min = %lf",min);
+        sum += sin_value;
+        // printf("sum = %lf",sum);
+        // printf("\n");
+    }
+
+    printf("%.2f %.2f %.2f", min, max, sum/n);
+
+
+    return 0;
+}
