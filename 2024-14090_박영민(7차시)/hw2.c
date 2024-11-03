bdf294affbccc3e0ed713eca6cd76c03634bb4bb
Ppine <tomy050213@naver.com>
2024-11-03 16:18:20 +0900

    hw2 complete

--- 2024-14090_박영민(7차시)/hw2.c
+++ 2024-14090_박영민(7차시)/hw2.c
@@ -20,9 +20,33 @@ int main() {
 
     // TODO: Implement below
 
+    char name1[50],name2[50];
+    scanf("%s %s",name1,name2);
+
+    Person *p1=NULL, *p2=NULL;
+    
+    if(strcmp(name1, alice.name)==0) p1=&alice;
+    else if(strcmp(name1, bob.name)==0) p1=&bob;
+    else if(strcmp(name1, charlie.name)==0) p1=&charlie;
+    else if(strcmp(name1, daisy.name)==0) p1=&daisy;
+
+    if(strcmp(name2, alice.name)==0) p2=&alice;
+    else if(strcmp(name2, bob.name)==0) p2=&bob;
+    else if(strcmp(name2, charlie.name)==0) p2=&charlie;
+    else if(strcmp(name2, daisy.name)==0) p2=&daisy;
+
+    if(is_ancestor(p1,p2)) printf("%s is an ancestor of %s",name1, name2);
+    else printf("%s is not an ancestor of %s",name1, name2);
+
+
+
     return 0;
 }
 
 int is_ancestor(Person* ancestor, Person* descendant) {
     // TODO: Implement this function
+    if(descendant == NULL) return 0;
+    if(descendant == ancestor) return 1;
+    return is_ancestor(ancestor, descendant->parent);
+
 }
--- 2024-14090_박영민(7차시)/tempCodeRunnerFile.c
+++ 2024-14090_박영민(7차시)/tempCodeRunnerFile.c
@@ -1,4 +1,4 @@
-)
-    // {
-    //     printf("%d",i);
-    // }
+s_ancestor(Person* ancestor, Person* descendant) {
+    // TODO: Implement this function
+    printf("%s %s", ancestor->parent, descendant->parent);
+}
