3460500e905a620a8bc525cd7d971686da01631b
Ppine <tomy050213@naver.com>
2024-11-12 13:55:58 +0900

    del gpt and fin hw1

--- 2024-14090_박영민(8차시)/hw1.c
+++ 2024-14090_박영민(8차시)/hw1.c
@@ -9,16 +9,78 @@ typedef struct student
     struct stu* next;
 }stu;
 
-stu* new_stu(const char *name, int score)
+stu* create(const char *name, int score)
 {
     stu *new_stu = (stu*)malloc(sizeof(stu));
     if(new_stu)
+    {
+        strncpy(new_stu -> name, name, 49);
+        new_stu->name[49]='\0';
+        new_stu->score = score;
+        new_stu->next = NULL;
+    }
+    return new_stu;
 }
 
+void append_student(stu **head, const char*name, int score)
+{
+    stu *new_stu = create(name,score);
+    if(!new_stu) return;
+
+    if(*head == NULL) *head = new_stu;
+    else
+    {
+        stu *temp = *head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_stu;
+    }
+}
+
+void print_student(stu *head)
+{
+    if(head == NULL) printf("Empty\n");
+    else
+    {
+        stu *temp = head;
+        while(temp!=NULL)
+        {
+            printf("%s %d\n",temp->name, temp->score);
+            temp = temp->next;
+        }
+    }
+}
+
+int get_score(stu *head, const char *name)
+{
+    stu *temp = head;
+    while (temp!=NULL)
+    {
+        if(strcmp(temp->name, name)==0) return temp->score;
+        temp = temp->next;
+    }
+    return -1;
+}
+
+void free_list(stu *head)
+{
+    stu *temp;
+    while(head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
 
 int main()
 {
+    stu *head = NULL;
     int n;
+    int score;
+    char name[50];
     
     while(1)
     {
@@ -26,17 +88,19 @@ int main()
         if(n==0) return 0;
         else if(n==1)//append
         {
-            append_stu();
-            // printf("append\n");
+            scanf("%s %d",name,&score);
+            append_student(&head, name,score);
         }
-        // else if(n==2)//print list
-        // {
-        //     print_stu();
-        // }
-        // else if(n==3)//get name and print score
-        // {
-        //     get_stu();
-        // }
+        else if(n==2)//print list
+        {
+            print_student(head);
+        }
+        else if(n==3)//get name and print score
+        {
+            scanf("%s",name);
+            int result = get_score(head,name);
+            printf("%d\n",result);
+        }
         else printf("Invalid Input\n");
     }
     return 0;
