8f7d8a5065e1848068e873acab6e420cc21b7b51
Ppine <tomy050213@naver.com>
2024-12-02 17:16:11 +0900

    hw2?

--- 2024-14090_박영민(10차시)/hw2_skeleton.c
+++ 2024-14090_박영민(10차시)/hw2_skeleton.c
@@ -2,7 +2,7 @@
 
 // TODO : Fix the macros
 #define SQUARE(x) x * x 
-#define MIN(a, b) a < b ? a : b 
+#define MIN(a, b) a < b ? (2*a) : (2*b)
 #define MULTIPLY(x, y) x * y 
 
 int main() {
@@ -10,9 +10,9 @@ int main() {
     int a = 20, b = 10;
 
     printf("num = %d, a = %d, b = %d\n", num, a, b);
-    printf("Square of (num + num): %d\n", SQUARE(num + num)); 
-    printf("Twice the minimum of (a + 5) and (b - 5): %d\n", 2 * MIN(a + 5, b - 5)); 
-    printf("Multiply (a + 1) and (b - 1): %d\n", MULTIPLY(a + 1, b - 1));
+    printf("Square of (num + num): %d\n", SQUARE((num + num))); 
+    printf("Twice the minimum of (a + 5) and (b - 5): %d\n", 2 * MIN((a + 5), (b - 5))); 
+    printf("Multiply (a + 1) and (b - 1): %d\n", MULTIPLY((a + 1), (b - 1)));
     return 0;
 }
 
